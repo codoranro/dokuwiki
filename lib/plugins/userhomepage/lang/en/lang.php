8bb45f7a413c5d80bd03d0048e1d8cf33b1693ac
Dmitry Isakov <idm@seal.astral.perm.ru>
2017-02-02 12:53:05 +0500

    Добавлен базовый UHP и bootstrap3

---
+++ lib/plugins/userhomepage/lang/en/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * English language file for Userhomepage plugin
+ * @author     Simon DELAGE <simon.geekitude@gmail.com>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+    $lang['copyerror'] = 'Copy error';
+    $lang['copysuccess'] = 'Successfull copy';
+    $lang['copynotneeded'] = 'Copy not needed';
+    $lang['createdprivatens'] = 'Created your private namespace';
+    $lang['createdpublicpage'] = 'Created your public page';
+    $lang['privatenamespace'] = 'Private Space';
+    $lang['publicpage'] = 'Public Page';
+    $lang['uhpcreated'] = 'Automatic creation by User Homepage plugin';
+    $lang['fromskeleton'] = 'Added this page from User Homepage skeleton to your private namespace:';
+    $lang['aclupdate'] = 'ACL rules added or updated:';
+    $lang['mkdirfailure'] = 'Creating this directory failed:';
+    $lang['settingsconflict'] = 'UserHomePage settings conflict! Make sure Private and Public namespaces are different. Plugin will have no effect untill this is corrected.';
